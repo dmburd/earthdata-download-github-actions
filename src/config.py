ef201b406d726b30a47d07c1270e0d36dfe181bf
Dmitry Burdeiny <dmitry.burdeiny@gmail.com>
2026-03-18 21:11:09 +0100

    added env vars from repo secrets and vars

--- src/config.py
+++ src/config.py
@@ -1,17 +1,6 @@
 import os
 
-from dotenv import load_dotenv
-from nanoid_dictionary import lowercase, numbers, uppercase
-
-NANOID_ALPHABET = numbers + uppercase + lowercase
-NANOID_SIZE = 21
-
-load_dotenv()
-
 SAVED_RESULTS_ROOTDIR = os.getenv("SAVED_RESULTS_ROOTDIR")
-
-NTFY_SH_TOPIC_URL = os.getenv("NTFY_SH_TOPIC_URL")
-
 EARTHDATA_LOGIN = os.getenv("EARTHDATA_LOGIN")
 EARTHDATA_PASSWORD = os.getenv("EARTHDATA_PASSWORD")
 EDL_TOKEN = os.getenv("EDL_TOKEN")
