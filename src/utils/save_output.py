f4ca2e2cd41e314cc10d207f1cf22ac5edf8a579
Dmitry Burdeiny <dmitry.burdeiny@gmail.com>
2026-03-19 20:40:30 +0100

    save to b2 bucket respecting relpaths

--- src/utils/save_output.py
+++ src/utils/save_output.py
@@ -6,13 +6,14 @@ from bokeh.layouts import column
 from bokeh.plotting import output_file, save
 from loguru import logger
 
-from src.config import LOCAL_SAVED_RESULTS_ROOTDIR
+from src.config import LOCAL_SAVED_RESULTS_ROOTDIR, B2_SAVED_RESULTS_ROOTDIR
 from src.pydantic_models import (
     EarthdataDownloadVisualizeServiceRequest,
     VisualizationSettings,
 )
 from src.utils.common import extract_track_number_from_h5_url_or_fpath
 from src.utils.visualization import visualize_single_track
+from src.utils.b2 import B2_BUCKET
 
 
 def save_output_dict_structure(
@@ -37,6 +38,13 @@ def save_output_dict_structure(
 
     with open(output_path / "output_dict_structure.json", "w") as f:
         f.write("\n".join(output_str_lines) + "\n")
+
+    relpath = output_path.relative_to(LOCAL_SAVED_RESULTS_ROOTDIR)
+
+    B2_BUCKET.upload_local_file(
+        local_file=output_path / "output_dict_structure.json",
+        file_name=relpath / "output_dict_structure.json",
+    )
 
 
 def save_few_tracks_visualized(
@@ -79,6 +87,13 @@ def save_few_tracks_visualized(
     else:
         logger.info("No track figures were generated")
 
+    relpath = output_path.relative_to(LOCAL_SAVED_RESULTS_ROOTDIR)
+
+    B2_BUCKET.upload_local_file(
+        local_file=output_path / "few_tracks_visualized.html",
+        file_name=relpath / "few_tracks_visualized.html",
+    )
+
 
 def save_output_files(
     request_params: EarthdataDownloadVisualizeServiceRequest,
@@ -95,14 +110,15 @@ def save_output_files(
     with open(output_path / "input_request.json", "w") as f:
         json.dump(request_params.model_dump(), f, default=str, indent=4)
 
-    relpath = (output_path / "input_request.json").relative_to(LOCAL_SAVED_RESULTS_ROOTDIR)
-    print(f"{relpath=}")
+    relpath = output_path.relative_to(LOCAL_SAVED_RESULTS_ROOTDIR)
+
+    B2_BUCKET.upload_local_file(
+        local_file=output_path / "input_request.json",
+        file_name=relpath / "input_request.json",
+    )
 
     # (2) Save output dict structure
     save_output_dict_structure(track_fname_to_arr_dict, output_path)
 
     # (3) Save few tracks visualized
     save_few_tracks_visualized(request_params, track_fname_to_arr_dict, output_path)
-
-    relpath = output_path.relative_to(LOCAL_SAVED_RESULTS_ROOTDIR)
-    print(f"{relpath=}")
